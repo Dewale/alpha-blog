3ae07ca434e6d97c004291d47cef55c916dd5e77
Adewale Adekoya <walekoya125@gmail.com>
2017-05-06 22:43:34 +0000

    Integration test on Categories

---
+++ app/models/category.rb
@@ -0,0 +1,4 @@
+class Category < ActiveRecord::Base
+    validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+    #validates_uniqueness_of :name
+end
